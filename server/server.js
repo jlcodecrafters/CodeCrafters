b626be562c50665f328d3d322c863a6517475549
jlcodecrafters <jlcodecrafters@gmail.com>
2024-05-25 00:32:12 -0700

    update

--- server/server.js
+++ server/server.js
@@ -48,5 +48,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
