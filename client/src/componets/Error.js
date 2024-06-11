a9fa0853987f2de1c856f197421757444e3a4b43
jlcodecrafters <jlcodecrafters@gmail.com>
2024-06-11 14:26:23 -0700

    update

--- client/src/componets/Error.js
+++ client/src/componets/Error.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../styles/error.css';
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
 export const Error = () => {
   return (
@@ -9,12 +9,14 @@ export const Error = () => {
       <Helmet>
         <title>Error 404 Page</title>
         <link rel='canonical' href='https://www.jlcodecrafters.com/404' />
+        <meta name="robots" content="noindex" />
       </Helmet>
-        <div className='error'>
-            <h1 className='TextAcenter'>Error: This page DOES NOT EXIST.</h1>
-            <button><Link to='/' >Click to return to Home.</Link></button>
-            
-        </div>
+      <div className='error'>
+        <h1 className='TextAcenter'>Error: This page DOES NOT EXIST.</h1>
+        <button>
+          <Link to='/'>Click to return to Home.</Link>
+        </button>
+      </div>
     </div>
   )
 }
